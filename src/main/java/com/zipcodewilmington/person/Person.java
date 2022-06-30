38fc29a5a765e05badf5cc2428cae5b93f97a7cc
Ryan <ryan@zipcodersmbp55.lan>
2022-06-29 21:58:44 -0400

    filled out constructors stubs

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -1,4 +1,5 @@
 package com.zipcodewilmington.person;
+import java.util.*;
 
 /**
  * Created by leon on 2/12/18.
@@ -6,30 +7,73 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private int birthYear;
+    private String birthPlace;
+    private String hairColor;
+    private String eyeColor;
+    private List<String> skills;
 
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
+
+    public String getBirthPlace() {return this.birthPlace;}
+
+    public Integer getBirthYear() {return this.birthYear;}
+
+    public String getHairColor() {return this.hairColor;}
+
+    public String getEyeColor() {return this.eyeColor;}
+
+    public List<String> getSkills() {return this.skills;}
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -1,5 +1,5 @@
 package com.zipcodewilmington.person;
-
+import java.util.*;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,6 +7,7 @@ import org.junit.Test;
  * Created by leon on 2/12/18.
  */
 public class TestPerson {
+
     @Test
     public void testDefaultConstructor() {
         // Given
@@ -93,6 +94,76 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetBirthYear() {
+        // Given
+        Person person = new Person();
+        Integer expected = 1977;
+
+        // When
+        person.setBirthYear(expected);
+
+        // Then
+        Integer actual = person.getBirthYear();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetBirthPlace() {
+        // Given
+        Person person = new Person();
+        String expected = "South Dakota";
+
+        // When
+        person.setBirthPlace(expected);
+
+        // Then
+        String actual = person.getBirthPlace();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetHairColor() {
+        // Given
+        Person person = new Person();
+        String expected = "brown";
+
+        // When
+        person.setHairColor(expected);
+
+        // Then
+        String actual = person.getHairColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetEyeColor() {
+        // Given
+        Person person = new Person();
+        String expected = "green";
+
+        // When
+        person.setEyeColor(expected);
+
+        // Then
+        String actual = person.getEyeColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetSkills() {
+        // Given
+        Person person = new Person();
+        List<String> expected = new ArrayList<String>();
+
+        // When
+        person.setSkills(expected);
+
+        // Then
+        List<String> actual = person.getSkills();
         Assert.assertEquals(expected, actual);
     }
 }
